9dda9329e02d08663089b8fbb27332f4a091969d
hoangcuongbk80 <hoangcuongbk80@gmail.com>
2024-10-23 20:02:51 +0700

    initial

---
+++ configs/DGNPose/ssHB/ss_dibr_mlBCE_FreezeBN_woCenter_woDepth_refinePM10_10e_train_diffRatio/ss_dibr_mlBCE_FreezeBN_woCenter_woDepth_refinePM10_10e_train720/ss_dibr_mlBCE_FreezeBN_woCenter_woDepth_refinePM10_10e_train720_driller.py
@@ -0,0 +1,17 @@
+_base_ = ["./ss_dibr_mlBCE_FreezeBN_woCenter_woDepth_refinePM10_10e_train720_benchvise.py"]
+
+# refiner_cfg_path = "configs/_base_/DGNPose_refiner_base.py"
+OUTPUT_DIR = "output/DGNPose/ssHB/ss_dibr_mlBCE_FreezeBN_woCenter_woDepth_refinePM10_10e_train720/driller"
+
+DATASETS = dict(
+    TRAIN=("hb_bdp_driller_train720",),  # real data
+    TRAIN2=("lm_pbr_driller_train",),  # synthetic data
+    TEST=("hb_bdp_driller_test100",),
+)
+
+RENDERER = dict(DIFF_RENDERER="DIBR")  # DIBR | DIBR
+
+MODEL = dict(
+    # synthetically trained model
+    WEIGHTS="output/DGNPose/lm_pbr/resnest50d_a6_AugCosyAAEGray_BG05_mlBCE_lm_pbr_100e/driller/model_final_wo_optim-4cfc7d64.pth",
+)
